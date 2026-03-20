2dd4162fa20657f45effc47163ff26339c4cfcdb
odon-airoldi <odon.airoldi@gmail.com>
2026-03-20 17:04:55 +0100

    api call for products use useEffect

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from 'react'
+
 export default function Products() {
+
+    const api_products = 'https://fakestoreapi.com/products'
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch(api_products)
+            .then(res => res.json())
+            .then(data => {
+
+                setProducts(data)
+
+            })
+    }, [])
+
+    console.log(products)
+
 
     return (
         <>
